64a93ddf588e7c29143fd2c29f398a09eaff88a9
Vadym <x6center@gmail.com>
2018-11-28 10:36:01 +0200

    obj

---
+++ script_4_lessons.js
@@ -0,0 +1,83 @@
+// var arr = [21, 3, 9, 45, 0 , 6] ;
+// var number = prompt("Enter ");
+// function namb (messege){ 
+//         if (arr.includes(messege)){
+            
+//         }else{
+//             arr.push(messege);
+//         }
+// }
+
+// console.log (masseg)
+
+
+
+
+// var arroject = [
+
+// {
+// title: 'car',
+// info: 'v',
+// },
+
+// {
+// title: 'age',
+// info: '5',
+// },
+// ]
+
+// arroject.map(function (item){
+//     console.log (item.title)
+// }
+// );
+
+// var obj = { 
+
+//     name: 'Илья',
+//     size: '<jkmijjq',
+// };
+// for (var key in obj) {
+//     if ( obj [key] === 'Илья');
+//     console.log ('key', obj[key]);
+// }
+
+
+// var user ={
+// first_name: 'Илья',
+// last_name: '',
+// age: '',
+// settings: [
+//         {
+//         sizeName: '',
+//         prodacts: [],
+//         }
+//     ]
+// }
+
+// var obj = {
+//     name: 'dsd',
+//     age: 'sdd',
+//     array: 'sd8',
+// };
+// for (var key in obj) {
+//     alert("ключ:" + key + "Значение:" + obj[key]);
+//         if ( obj [key] === '');
+//         console.log ( obj[key]);}
+
+
+
+
+// var obj = {
+//     Олег: '100',
+//     Вася: '200',
+//     Коля: '300',
+// };
+// var sum = 0;
+// for (var key in obj)
+// function objname (obj){
+  
+
+ 
+// }
+
+
